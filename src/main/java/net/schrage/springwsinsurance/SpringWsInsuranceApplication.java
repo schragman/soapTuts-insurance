dab16d30b36cacbf5fb67cb576339d0e2ea8d1de
mschrage <michael.schrage@fincon.eu>
2022-07-08 12:47:23 +0200

    First Commit

---
+++ src/main/java/net/schrage/springwsinsurance/SpringWsInsuranceApplication.java
@@ -0,0 +1,13 @@
+package net.schrage.springwsinsurance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWsInsuranceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringWsInsuranceApplication.class, args);
+  }
+
+}
---
+++ src/test/java/net/schrage/springwsinsurance/SpringWsInsuranceApplicationTests.java
@@ -0,0 +1,13 @@
+package net.schrage.springwsinsurance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWsInsuranceApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
